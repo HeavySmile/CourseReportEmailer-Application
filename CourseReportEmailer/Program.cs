87123e6a6413ca12030fd19abfdf450dfe0dd9f5
Leonid Shubin <lenyashubin2002@gmail.com>
2023-07-11 13:54:17 +0300

    Added base Mail Send logic

--- CourseReportEmailer/Program.cs
+++ CourseReportEmailer/Program.cs
@@ -1,11 +1,34 @@
 ﻿
+using CourseReportEmailer.Commands;
+using CourseReportEmailer.Models;
+using CourseReportEmailer.Workers;
+using Newtonsoft.Json;
+using System.Data;
+using System.Net.Mail;
+using System.Net;
+
 namespace CourseReportEmailer
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                EnrollmentDetailReportCommand command = new EnrollmentDetailReportCommand(@"Data Source=localhost;Initial Catalog=CourseReport;Integrated Security=True");
+                IList<EnrollmentDetailReportModel> models = command.GetList();
+
+                var reportFileName = "EnrollmentDetailsReport.xlsx";
+                EnrollmentDetailReportSpreadSheetCreator enrollmentSheetCreator = new EnrollmentDetailReportSpreadSheetCreator();
+                enrollmentSheetCreator.Create(reportFileName, models);
+
+                EnrollmentDetailReportEmailSender emailer = new EnrollmentDetailReportEmailSender();
+                emailer.Send(reportFileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something went wrong: {ex.Message}");
+            }
         }
     }
 }
